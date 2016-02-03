0fbd090df2ce48937b3fc2668a2d51a90555d5ef
pythonic64 <pythonic64@gmail.com>
2016-02-03 20:14:04 +0100

    Using setuptools instead of distutils.

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
 
 import spinekivy
-
-
-packages = [
-    'spinekivy'
-]
-
-package_dir = {
-    'spinekivy': 'spinekivy'
-}
 
 
 setup(
     name='spinekivy',
     version=spinekivy.__version__,
-    author='Tileworks Games',
+    author='Tileworks Games and other contributors',
     author_email='tileworksgames@gmail.com',
-    description='Spine renderer for kivy.',
+    description='Spine skeleton renderer for Kivy framework.',
     long_description=spinekivy.__doc__,
     license='MIT',
-    packages=packages,
-    package_dir=package_dir
+    packages=['spinekivy'],
+    package_dir={
+        'spinekivy': 'spinekivy'
+    },
+    install_requires=[
+        'kivy>=1.8.0',
+        'spine-cython>=0.5.0'
+    ],
+    keywords='spine kivy',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Games/Entertainment',
+        'Topic :: Multimedia :: Graphics',
+    ]
 )
